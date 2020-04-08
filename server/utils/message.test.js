70d7d4ccea40bb28d31a42e0680e9bd9a67692da
clicktravel-craig <craig.handley@clicktravel.com>
2020-04-08 10:04:36 +0100

    add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,20 @@ describe('generateMessage', () => {
         expect(message.createdAt).toBeGreaterThanOrEqual(0);
         expect(message.from).toBe(from);
         expect(message.text).toBe(text);
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Craig';
+        var lat = 15;
+        var long = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+
+        var message = generateLocationMessage(from, lat, long);
+
+        expect(message.createdAt).toBeGreaterThanOrEqual(0);
+        expect(message.from).toBe(from);
+        expect(message.url).toBe(url);
     });
 });
