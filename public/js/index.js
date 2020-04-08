ede1b2ab76e6d50694a4adf6793fe0693db8287d
clicktravel-craig <craig.handley@clicktravel.com>
2020-04-08 13:42:55 +0100

    scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 var locationButton = jQuery('#send-location');
 
 socket.on('connect', function () {
@@ -18,6 +33,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var formattedTime = moment(message.createdAt).format('h:mm a');
     // var li = jQuery('<li></li>');
@@ -34,6 +50,7 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');
--- server/server.js
+++ server/server.js
@@ -27,7 +27,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
     });
